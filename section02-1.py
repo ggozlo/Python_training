363cf6b8dfe622be56fd61395e104582001c3aa1
ggozlo <78086722+ggozlo@users.noreply.github.com>
2021-02-03 23:04:50 +0900

    no

--- section02-1.py
+++ section02-1.py
@@ -10,16 +10,17 @@ print('''Hello Python!''')
 print()
 
 # separator 옵션
+print ('T', 'E','S','T')
 print ('T', 'E','S','T', sep='')
 print ( 'T'+'E'+'S'+'T')
 print('2019','02','19',sep='-')
 print('niceman','google.com',sep='@')
 
 # end 옵션 사용
-print('Welcome to', end='')
-print(' the black paradise', end='\n')
-print('piano notes\n')
+print('Welcome to', end='-')
+print(' the new paradise', end = "***")
 
+print()
 # format 사용
 print('{} and {}'.format('I', 'You'))
 print("{0} and {1} and {0}".format('You', 'Me'))
@@ -28,7 +29,7 @@ print("{a} are {b}".format(a = 'you',b='Me'))
 print("%s favorite number is %d" %('My', 7)) # %s : 문자, %d : 정수, %f : 실수
 
 print("Test1: %5d, Price: %4.2f" %(777, 6534.123) )
-print("Test1: {0: 5d}, Prcie:{1: 4.2f}".format(776, 6534.123))
+print("Test1: {0: 5d}, Prcie:{1: 6.2f}".format(776, 6534.123))
 print("Test1: {a: 5d}, Price:{b: 4.2f}".format(a=776, b=6534.123))
 
 # \n : 개행 ( 줄넘김 )
@@ -46,3 +47,6 @@ print('test\n')
 print('test')
 
 print('t\te\ts\tt ')
+
+a = 10 
+print(a)
